8bfcf52a016d8d35f185b48db07ebe9ef066c171
huynt7 <tuanhuycntt@gmail.com>
2023-05-25 17:17:08 +0700

    up

--- res/js/direct.js
+++ res/js/direct.js
@@ -25,10 +25,10 @@ function go(){
 	
 	
 	if (!result) {
-outputBox.value = "";
-setStatus("Error: Invalid URL", true);
-outputBox.disabled = true;
-return;
+		outputBox.value = "";
+		setStatus("Error: Invalid URL", true);
+		outputBox.disabled = true;
+		return;
 	}
 	
 	var finalLink = "https://drive.google.com/uc?export=download&id=" + result[1];
@@ -51,3 +51,39 @@ setStatus("Link copied to clipboard!");
 setStatus("Couldn't copy link to clipboard. Please copy it manually.", true);
 	}
 }
+
+$(document).ready(function(){
+	$("#but").click(function(){
+		var add = $("#cde").val();
+		var drive = add.indexOf("google.com/file/d/index.html");
+		var drive2 = add.indexOf("google.com/open");
+		var dbox = add.indexOf("dropbox.com/s");
+		var odrive = add.indexOf("onedrive.live.com");
+		if (drive != -1) {
+			var start = add.indexOf("d/index.html");
+			var end = add.indexOf("/view");
+			var reString = add.slice(start + 2, end);
+			var link = "https://drive.google.com/uc?export=download&id=" + reString + "";
+			$("#linkpaste").val(link);
+			$("#linkpaste").select();
+		} else if (drive2 != -1) {
+			var start = add.indexOf("id=");
+			var end = add.length;
+			var reString = add.slice(start+3, end);
+			var link = "https://drive.google.com/uc?export=download&id=" + reString + "";
+			$("#linkpaste").val(link);
+			$("#linkpaste").select();
+		} else if (dbox != -1) {
+			var link = add.replace("?dl=0", "?dl=1");
+			$("#linkpaste").val(link);
+			$("#linkpaste").select();
+		} else if (odrive != -1) {
+			var link = add.replace("redir", "download");
+			var link = add.replace("embed", "download");
+			$("#linkpaste").val(link);
+			$("#linkpaste").select();
+		} else {
+			$("#linkpaste").val('Xin lỗi, có vẻ như link bạn vừa dán chưa chia sẻ công khai hoặc không đúng! Vui lòng kiểm tra lại!');
+		}
+	});
+});
